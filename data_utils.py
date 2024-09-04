86d1995e3f7428d3f3a31036d57f3dd8317373cf
paulruvolo <paullundyruvolo@gmail.com>
2024-09-04 14:13:08 -0400

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,7 @@
 import gdown
+from os import mkdir, system
 
 def download_dessert_test_data():
     gdown.download(id="1k_KaPCCO37HC8eqpd0D0JhKyGhqM8_KL", output="test_data.zip")
-    !mkdir test_data
-    !unzip -q test_data.zip -d test_data
+    mkdir('test_data')
+    system('unzip -q test_data.zip -d test_data')
